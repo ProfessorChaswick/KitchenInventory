73d8915f5cdc0f425a8ca6278e9368a8e7312395
ProfessorChaswick <112112334+ProfessorChaswick@users.noreply.github.com>
2023-05-11 18:54:47 -0400

    Added code to pipe *ngfor to include an "orderBy" format

--- UI/kitchen/src/app/app.component.ts
+++ UI/kitchen/src/app/app.component.ts
@@ -1,6 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Pipe, PipeTransform } from '@angular/core';
 import { KitchenService } from './service/kitchen.service';
 import { kitchen } from './models/kitchen.model';
+import { orderBy } from 'lodash';
+
+@Pipe({
+  name: "orderBy"
+ })
+ export class OrderByPipe implements PipeTransform {
+  transform(array: any, sortBy: string, order?: string): any[] {
+  const sortOrder = order ? order : 'asc'; // setting default ascending order
+ 
+   return orderBy(array, [sortBy], true);
+   }
+ }
 
 @Component({
   selector: 'app-root',
